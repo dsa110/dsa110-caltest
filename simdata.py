d42098879fc11ddf9aeef8fdcbe2964330dc09fd
Casey Law <caseyjlaw@gmail.com>
2019-09-05 10:30:36 -0700

    make scripts into module functions

--- simdata.py
+++ simdata.py
@@ -1,43 +1,62 @@
 import casatools as ct
 
-x = [0, 400, 380, 370, 410, 420, 200, -200, -400, -980, -550, -1250, -1200, -1200, -1350]
-y = [-1050, -500, 350, 800, 975, 1120, 1100, 1050, 950, 775, 1000, 1120, 575, -800, -900]
-names = ['DSA-101', 'DSA-102', 'DSA-103', 'DSA-104', 'DSA-105', 'DSA-106', 'DSA-107', 'DSA-108', 'DSA-109', 'DSA-110', 'DSA-111', 'DSA-112', 'DSA-113', 'DSA-114', 'DSA-115']
 
-sm = ct.simulator()
-sm.open('dsa110-calsrc.ms')
+def gensources(complist='src.cl'):
+    """ Create component list
+    """
 
-me = ct.measures()
-refpos = me.observatory('OVRO_MMA')                                                                                                                                                
+    cl = ct.componentlist()
+    caldirection = "J2000 12h00m00.0s 50d00m00.0s"
+    cl.addcomponent(dir=caldirection, flux=1.0, fluxunit='Jy', freq='1.4GHz', shape="Gaussian", 
+                    majoraxis="1arcsec", minoraxis='1arcsec', positionangle='0deg')
+    srcdirection = "J2000 12h30m00.0s 50d00m00.0s"
+    cl.addcomponent(dir=srcdirection, flux=1.0, fluxunit='Jy', freq='1.4GHz', shape="Gaussian", 
+                    majoraxis="1arcsec", minoraxis='1arcsec', positionangle='0deg')
+    cl.rename(complist)
+    cl.done()
 
-# TODO: confirm coordinates and set offsets from OVRO_MMA
-sm.setconfig(telescopename='DSA-110', x=x, y=y, dishdiameter=[5.0]*len(x), z=[0.]*len(x), offset=[0.0],
-             mount=['ALT-AZ'], antname=names, padname=names, coordsystem='local', referencelocation=refpos)
 
-sm.setspwindow(spwname='LBand', freq='1.4GHz', deltafreq='0.5MHz', freqresolution='0.5MHz', nchannels=1, stokes='XX YY')
-sm.settimes(integrationtime='1s', usehourangle=True, referencetime=58722)
-#sm.setnoise(simplenoise='0.1Jy')
-sm.setfeed(mode='perfect X Y')
-#sm.setgain(mode='fbm', amplitude=0.1)
-sm.setauto(autocorrwt=0.0)
+def simulate(complist='src.cl', outms='dsa110-calsrc.ms'):
+    """ Use source model to generate simulated ms
+    """
 
-vp = ct.vpmanager()
-vp.reset()
-vp.setpbairy(telescope="DSA-110", dishdiam="5m", maxrad="10deg", blockagediam="1m")
-sm.setvp(dovp=True, usedefaultvp=False)
+    x = [0, 400, 380, 370, 410, 420, 200, -200, -400, -980, -550, -1250, -1200, -1200, -1350]
+    y = [-1050, -500, 350, 800, 975, 1120, 1100, 1050, 950, 775, 1000, 1120, 575, -800, -900]
+    names = ['DSA-101', 'DSA-102', 'DSA-103', 'DSA-104', 'DSA-105', 'DSA-106', 'DSA-107', 'DSA-108', 'DSA-109', 'DSA-110', 'DSA-111', 'DSA-112', 'DSA-113', 'DSA-114', 'DSA-115']
 
-caldirstr = "J2000 12h00m00.0s 50d00m00.0s"
-caldir = me.direction(*caldirstr.split())
-sm.setfield(sourcename='cal', sourcedirection=caldir)
-srcdirstr = "J2000 12h30m00.0s 50d00m00.0s"
-sourcedir = me.direction(*srcdirstr.split())
-sm.setfield(sourcename='src', sourcedirection=sourcedir)
-sm.observe(sourcename='cal', spwname='LBand', starttime='-450s', stoptime='450s')  # times are in HA referenced to first source
-sm.observe(sourcename='src', spwname='LBand', starttime='1350s', stoptime='2250s')
+    sm = ct.simulator()
+    sm.open(outms)
 
-sm.predict(complist='srcs.cl')
+    me = ct.measures()
+    refpos = me.observatory('OVRO_MMA')                                                                                                                                                
+    # TODO: confirm coordinates and set offsets from OVRO_MMA
+    sm.setconfig(telescopename='DSA-110', x=x, y=y, dishdiameter=[5.0]*len(x), z=[0.]*len(x), offset=[0.0],
+                 mount=['ALT-AZ'], antname=names, padname=names, coordsystem='local', referencelocation=refpos)
 
-#sm.corrupt()
+    sm.setspwindow(spwname='LBand', freq='1.4GHz', deltafreq='0.5MHz', freqresolution='0.5MHz', nchannels=1, stokes='XX YY')
+    sm.settimes(integrationtime='1s', usehourangle=True, referencetime=58722)
+    #sm.setnoise(simplenoise='0.1Jy')
+    sm.setfeed(mode='perfect X Y')
+    #sm.setgain(mode='fbm', amplitude=0.1)
+    sm.setauto(autocorrwt=0.0)
 
-sm.summary()
-sm.done()
+    vp = ct.vpmanager()
+    vp.reset()
+    vp.setpbairy(telescope="DSA-110", dishdiam="5m", maxrad="10deg", blockagediam="1m")
+    sm.setvp(dovp=True, usedefaultvp=False)
+
+    caldirstr = "J2000 12h00m00.0s 50d00m00.0s"
+    caldir = me.direction(*caldirstr.split())
+    sm.setfield(sourcename='cal', sourcedirection=caldir)
+    srcdirstr = "J2000 12h30m00.0s 50d00m00.0s"
+    sourcedir = me.direction(*srcdirstr.split())
+    sm.setfield(sourcename='src', sourcedirection=sourcedir)
+
+    sm.observe(sourcename='cal', spwname='LBand', starttime='-450s', stoptime='450s')  # times are in HA referenced to first source
+    sm.observe(sourcename='src', spwname='LBand', starttime='1350s', stoptime='2250s')
+
+    sm.predict(complist=complist)
+    #sm.corrupt()
+
+    sm.summary()
+    sm.done()
