c368552d1dd1c8baf1608d70a18dee99f6525b4b
Casey Law <caseyjlaw@gmail.com>
2019-09-09 16:50:09 -0700

    working sim with fits pls cl. can clean it

--- simdata.py
+++ simdata.py
@@ -2,7 +2,9 @@ import logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
-import casatools as ct
+import matplotlib.pyplot as plt
+import casatools as tools
+import casatasks as tasks
 import os.path
 import shutil
 
@@ -18,7 +20,7 @@ def gensources(complist='src.cl', caldirection="J2000 12h00m00.0s 50d00m00.0s", 
         shutil.rmtree(complist)
 
     # assume compact sources
-    cl = ct.componentlist()
+    cl = tools.componentlist()
     cl.addcomponent(dir=caldirection, flux=calflux, fluxunit='Jy', freq=freq, shape="Gaussian", 
                     majoraxis="1arcsec", minoraxis='1arcsec', positionangle='0deg')
     if srcdirection is not None:
@@ -27,14 +29,16 @@ def gensources(complist='src.cl', caldirection="J2000 12h00m00.0s 50d00m00.0s", 
     cl.rename(complist)
     cl.done()
 
-# creating FIRST model image
-# Note that header from FIRST archive only has two axes. Need to force definition on import.
-# casatasks.importfits(fitsimage='first_12h+50d.fits', imagename='first_12h+50d.ms', beam=["5arcsec", "5arcsec", "0deg"], overwrite=True, defaultaxes=True, defaultaxesvalues=[180.0, 50.0, 1400000000.0, 'I'])
+# FIRST model image
+# > hdulist = astroquery.skyview.SkyView.get_images(position=co, survey='VLA FIRST (1.4 GHz)', height=2*u.deg, width=2*u.deg, pixels=7200)      
+# Header from FIRST archive only has two axes. Need to force definition on import.
+# > casatasks.importfits(fitsimage='first_12h+50d.fits', imagename='first_12h+50d.ms', beam=["5arcsec", "5arcsec", "0deg"], overwrite=True, defaultaxes=True, defaultaxesvalues=[180.0, 50.0, 1400000000.0, 'I'])
 
 def simulate(imagename='', complist='', msname='dsa110-calsrc.ms', freq='1.4GHz', integrationtime='1s',
-             diameter=5.0, noise='0Jy', gainnoise=0.,
+             diameter=5.0, noise='0Jy', gainnoise=0., nchan=1,
              calobsdir = "J2000 12h00m00.0s 50d00m00.0s", srcobsdir="J2000 12h30m00.0s 50d00m00.0s"):
     """ Use source model to generate simulated ms for a few DSA antennas.
+    If imagename and complist both provided, then complist will be added to image.
     """
 
     # outriggers
@@ -46,23 +50,23 @@ def simulate(imagename='', complist='', msname='dsa110-calsrc.ms', freq='1.4GHz'
         logger.info("Removing existing file, {0}".format(msname))
         shutil.rmtree(msname)
 
-    sm = ct.simulator()
+    sm = tools.simulator()
     sm.open(msname)
 
-    me = ct.measures()
+    me = tools.measures()
     refpos = me.observatory('OVRO_MMA')                                                                                                                                                
     # TODO: confirm coordinates and set offsets from OVRO_MMA
     sm.setconfig(telescopename='DSA-110', x=x, y=y, dishdiameter=[diameter]*len(x), z=[0.]*len(x), offset=[0.0],
                  mount=['ALT-AZ'], antname=names, padname=names, coordsystem='local', referencelocation=refpos)
 
-    sm.setspwindow(spwname='LBand', freq=freq, deltafreq='0.5MHz', freqresolution='0.5MHz', nchannels=1, stokes='XX YY')
+    sm.setspwindow(spwname='LBand', freq=freq, deltafreq='0.5MHz', freqresolution='0.5MHz', nchannels=nchan, stokes='XX YY')
     sm.settimes(integrationtime=integrationtime, usehourangle=True, referencetime=58722)
     sm.setfeed(mode='perfect X Y')
     sm.setauto(autocorrwt=0.0)
 
-    vp = ct.vpmanager()
+    vp = tools.vpmanager()
     vp.reset()
-    vp.setpbairy(telescope="DSA-110", dishdiam="5m", maxrad="10deg", blockagediam="1m")
+    vp.setpbairy(telescope="DSA-110", dishdiam="{0}m".format(diameter), maxrad="10deg", blockagediam="1m")
     sm.setvp(dovp=True, usedefaultvp=False)
 
     if calobsdir is not None:
@@ -75,10 +79,19 @@ def simulate(imagename='', complist='', msname='dsa110-calsrc.ms', freq='1.4GHz'
     if srcobsdir is not None:
         sm.observe(sourcename='src', spwname='LBand', starttime='1350s', stoptime='2250s')  # 30min later
 
-    sm.predict(complist=complist, imagename=imagename)
-    sm.setnoise(mode='simplenoise', simplenoise=noise)
-    sm.setgain(mode='fbm', amplitude=gainnoise)
-    sm.corrupt()
+    if len(imagename) and len(complist):
+        sm.predict(imagename=imagename)
+        if len(complist):
+            sm.predict(complist=complist, incremental=True)
+    elif len(complist):
+        sm.predict(complist=complist)
+
+    if noise:
+        sm.setnoise(mode='simplenoise', simplenoise=noise)
+    if gainnoise:
+        sm.setgain(mode='fbm', amplitude=gainnoise)
+    if noise or gainnoise:
+        sm.corrupt()
 
     sm.summary()
     sm.done()
@@ -88,7 +101,7 @@ def read(msname='dsa110-calsrc.ms'):
     """ Read simulated ms and return data
     """
 
-    ms = ct.ms()                                                                                                                                                    
+    ms = tools.ms()                                                                                                                                                    
     ms.open('dsa110-calsrc.ms')                                                                                                                                     
     dd = ms.getdata(items=['data', 'axis_info', 'uvw'], ifraxis=True)                                                                                               
     data = dd['data']                                                                                                                                               
@@ -100,7 +113,7 @@ def read(msname='dsa110-calsrc.ms'):
 
 
 def solve(msname='dsa110-calsrc.ms', calname='cal.G', apply=False, show=True):
-    cb = ct.calibrater()
+    cb = tools.calibrater()
     cb.open(msname)
     cb.setsolve(type='G', t=900., table=calname, phaseonly=True, refant=0)
     cb.solve()
@@ -112,9 +125,13 @@ def solve(msname='dsa110-calsrc.ms', calname='cal.G', apply=False, show=True):
         cb.correct()
 
 
-def image(msname='dsa110-calsrc.ms'):
-    im = ct.imager() 
-    im.open(msname)
+def display(imname=None):
+    """ Show an image
+    """
 
-    im.defineimage(nx=128, ny=128) 
-    im.clean(image='test.im', complist='test.cl', niter=0)  # CRASH
+    im = tools.image()
+    im.open(imname)
+    data = im.getchunk()
+    fig = plt.figure(figsize=(10,8))
+    plt.imshow(data.squeeze(), origin='bottom', interpolation='nearest')
+    plt.show()
